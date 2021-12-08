682b6b7b5540e600d1da2024b3b4cfb93a6227dd
gabrielkf <gabrielkf@gmail.com>
2021-12-07 22:09:03 -0300

    Reference and Value types

--- tests/GradeBook.Tests/TypeTests.cs
+++ tests/GradeBook.Tests/TypeTests.cs
@@ -6,6 +6,50 @@ namespace GradeBook.Tests
     public class TypeTests
     {
         [Fact]
+        public void SetNameFromPointerValue()
+        {
+            var book1 = GetBook("Book 1");
+            SetName(book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        public void SetName(Book book, string name)
+        {
+            book.Name = name;
+        }
+
+
+        [Fact]
+        public void PassValueToMethodIsDefault()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookAndSetName(book1, "New Name");
+
+            Assert.Equal("Book 1", book1.Name);
+        }
+
+        public void GetBookAndSetName(Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+        [Fact]
+        public void PassReference()
+        {
+            var book1 = GetBook("Book 1");
+            GetBookRefAndSetName(ref book1, "New Name");
+
+            Assert.Equal("New Name", book1.Name);
+        }
+
+        public void GetBookRefAndSetName(ref Book book, string name)
+        {
+            book = new Book(name);
+        }
+
+
+        [Fact]
         public void GetBookReturnsDifferentObjects()
         {
             var book1 = GetBook("Book 1");
@@ -29,6 +73,40 @@ namespace GradeBook.Tests
         Book GetBook(string name)
         {
             return new Book(name);
+        }
+
+        [Fact]
+        public void OutPassesByValue()
+        {
+            // var x = GetInt();
+            SetInt(out var x);
+
+            Assert.Equal(42, x);
+        }
+
+        private void SetInt(out int x)
+        {
+            x = 42;
+        }
+
+        private int GetInt()
+        {
+            return 3;
+        }
+
+        [Fact]
+        public void StringsBehaveLikeValueTypes()
+        {
+            string name = "Gabriel";
+            var upperCase = MakeUpperCase(name);
+
+            Assert.Equal("Gabriel", name);
+            Assert.Equal("GABRIEL", upperCase);
+        }
+
+        private string MakeUpperCase(string str)
+        {
+            return str.ToUpper();
         }
     }
 }
